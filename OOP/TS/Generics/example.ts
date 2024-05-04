3df9408f6f1f4027bed902e3c885cc96bed0196d
hoqhuy <hoquanghuy0123@gmail.com>
2024-05-05 00:26:34 +0700

    [Add] practice generic

---
+++ OOP/TS/Generics/example.ts
@@ -0,0 +1,16 @@
+class HoldAnyThing<T> {
+    constructor(public data: T[]) {}
+
+    add(a: T) {
+        this.data.push(a);
+        return this.data;
+    }
+
+    get(index: number): T {
+        return this.data[index];
+    }
+}
+
+const holdNumber = new HoldAnyThing<number>([10, 20, 30]);
+holdNumber.add(10);
+console.log(holdNumber.get(2));
