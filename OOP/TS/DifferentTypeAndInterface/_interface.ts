7757fa073f5d1fef6c41d1906935df46b1e9d978
hqyuh <hoquanghuy0123@gmail.com>
2024-05-08 13:21:32 +0700

    [Add] different type and interface, practice tuple

---
+++ OOP/TS/DifferentTypeAndInterface/_interface.ts
@@ -0,0 +1,46 @@
+// merge => OK
+
+interface AA {
+    name: string;
+}
+
+interface AA {
+    age: number;
+}
+
+const a1: AA = {
+    name: 'HH',
+    age: 24
+}
+
+// computed properties => NO => error: A mapped type may not declare properties or methods.
+/*
+type keys = 'name' | 'age'
+
+interface Person {
+    [key in keys]: string
+}
+*/
+
+
+// intersection properties
+interface Name1 {
+    name: string
+};
+
+interface Age1 {
+    age: number
+};
+
+type Person1 = Name1 & Age1;
+
+
+
+// extend and implements
+interface Animal {
+    name: string
+}
+
+interface Bear extends Animal {
+    honey: boolean
+}
