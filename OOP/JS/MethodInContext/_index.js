1d5174429ce0d9d3c599fe63c5bcc7963a291830
hqyuh <hoquanghuy0123@gmail.com>
2024-07-02 15:40:41 +0700

    [Add] decorator

--- OOP/JS/MethodInContext/_index.js
+++ OOP/JS/MethodInContext/_index.js
@@ -9,13 +9,21 @@ const mySelf = obj.getLocation;
 // input here is undefined because the method obj.getLocation is assigned to the variable mySelf 
 // Ai this point, mySelf is just a reference to the getLocation function, 
 // but is not attached to the obj object
-console.log(mySelf());
+console.log(mySelf()); // => undefined => indirect
+console.log(obj.getLocation()) // DN => direct
 
 // Fix
 const mySelf1 = obj.getLocation.bind(obj);
 console.log(mySelf1());
 
 //------------------------------------------------------------//
+// const func1 = {};
+// const func2 = func1.bind(obj);
+// func2 => function () {
+//   call func1 and set this to obj
+// }
+//------------------------------------------------------------//
+
 // call 
 let personCall = { name: "Call" };
 function sayCall(a, b) {
--- OOP/TS/DataTypes/index.ts
+++ OOP/TS/DataTypes/index.ts
@@ -1,2 +1,18 @@
 // When you are not sure about what type you can assign? => Use unknown
 let ah: unknown = 2;
+
+const project = {
+  code: 100,
+  specification: {
+    areaSize: 100,
+    rooms: 3,
+  },
+} as const;
+
+type Specification = typeof project['specification'];
+
+const cca: Specification = {
+    areaSize: 100,
+    rooms: 3
+}
+console.log('🐔 =>  cca:', cca);
