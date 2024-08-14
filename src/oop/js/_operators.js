32ae887cb07c8cb4d2cf2fc5180648a3c91b8bdc
hqyuh <hoquanghuy0123@gmail.com>
2024-08-14 23:23:31 +0700

    [Add] practice operators

---
+++ src/oop/js/_operators.js
@@ -0,0 +1,14 @@
+let a = 5;
+let b = 10;
+
+// Logical AND
+// all true => true
+// 1 false  => false
+
+// Logical OR
+// 1 true   => true
+// all false => false
+
+console.log(a != b && a < b);
+console.log(a > b || a == b);
+console.log(a < b || a == b);
